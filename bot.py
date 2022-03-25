36261e46d0eb7b7a5cdf3d25e2ce2cfd1cbfa4f5
Bainshedone <84446371+Bainshedone@users.noreply.github.com>
2022-03-25 15:33:11 +0530

    Embed

--- bot.py
+++ bot.py
@@ -69,7 +69,9 @@ async def DM(ctx, user: discord.User, amount:int, *, message):
 async def pfp(ctx, user: discord.User):
   pfp = user.avatar_url
   user_id = user.id
-  await ctx.send(f"Hello Hooman!! a.k.a @{user_id}, here is your [avatar]('{pfp}&quality=loseless')")
+  embed_link = discord.Embed()
+  embed_link.description = f"Hello Hooman!! a.k.a @{user_id}, here is your [avatar]('{pfp}&quality=loseless')"
+  await ctx.send(embed=embed_link)
   
   
 @bot.command(name='ban',help='bans the user')
